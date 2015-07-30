50aaefede486c29404c0e670a2fb27d263a8cf57
majesiu <pawel.majewski@mail.com>
2015-07-30 23:44:28 +0200

    new update - visual index improvement - search by every element - modifiers for name search

--- app/models/SearchModel.scala
+++ app/models/SearchModel.scala
@@ -1,6 +1,7 @@
 package models
 
-import java.util.Date
+import java.text.SimpleDateFormat
+import java.util.{Calendar, Date}
 
 import anorm._
 import play.api.db.DB
@@ -9,17 +10,27 @@ import play.api.Play.current
 /**
  * Created by user on 2015-07-28.
  */
-case class SearchModel (name: Option[String], date: Option[Date], creator: Option[String]) {
+case class SearchModel (name: Option[String],nameOp: String, date: Option[Date], creator: Option[String]) {
   def toHtml = name + "\t" + date + "\t" + creator
 }
 
+
+
 object SearchModel{
+  val yearFormat = new SimpleDateFormat("yyyy-mm-dd")
 
   def findFilter(search: SearchModel): Seq[Event] = {
+    println
+    println
+    println("Sieeeeet: "+search.nameOp)
+      println
+      println
     DB.withConnection { implicit connection =>
       SQL("select * from Events " +
-        "where name like {name}").on(
-          'name -> search.name
+        "where name " + search.nameOp + " {name} and date > {date} and creator like {creator}").on(
+          'name -> search.name.getOrElse("%"),
+          'date -> search.date.getOrElse(Calendar.getInstance.getTime),
+          'creator -> search.creator.getOrElse("%")
         ).as(Event.simple *)
     }
   }
